34f6bc292c2d409aa1aa9902d646113aed72bcaf
hayato94087 <hayato94087@gmail.com>
2023-08-09 16:23:45 +0900

    Webhookの処理を実装

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  beforeAuth(req) {
+    // todo : do something
+  },
+  ignoredRoutes: ["/api/public/webhook"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
